541ae8a2369cb982593364b77702af5a7efae167
Davi Gabriel Scottini Adriano <davi.fadriano@gmail.com>
2024-11-10 18:23:36 -0300

    feat: Criando tela de edição

--- src/app/components/pages/user/edit-user/edit-user.component.ts
+++ src/app/components/pages/user/edit-user/edit-user.component.ts
@@ -1,12 +1,92 @@
-import { Component } from '@angular/core';
+import { User } from './../../../../service/user/user';
+import { Component, OnInit } from '@angular/core';
+import { MenuComponent } from '../../core/menu/menu.component';
+import { UserService } from '../../../../service/user/user.service';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors, ReactiveFormsModule } from '@angular/forms';
+import { lowercaseValidator, numberValidator, symbolValidator, uppercaseValidator } from '../../../../validators/password-validator';
+import { CommonModule } from '@angular/common';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-user',
   standalone: true,
-  imports: [],
+  imports: [
+    MenuComponent,
+    CommonModule,
+    ReactiveFormsModule,
+    NgbAlertModule
+  ],
   templateUrl: './edit-user.component.html',
   styleUrl: './edit-user.component.css'
 })
-export class EditUserComponent {
+export class EditUserComponent implements OnInit{
 
+  formulario!: FormGroup;
+  user!: User;
+
+  constructor (
+    private readonly userService: UserService,
+    private readonly formBuilder: FormBuilder,
+    private readonly router: Router,
+    private readonly route: ActivatedRoute
+  )
+  {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    this.userService.buscarPorId(parseInt(id!)).subscribe((result) => {
+      this.user = result.value;
+
+      this.formulario = this.formBuilder.group({
+        name: [this.user.name, [
+          Validators.required,
+          Validators.pattern(/^(?!\s*$)[a-zA-Z\s]+$/)
+        ]],
+        login: [this.user.login, [
+          Validators.required,
+          Validators.email
+        ]],
+        password: ['', [
+          Validators.required,
+          Validators.minLength(8),
+          uppercaseValidator(),
+          lowercaseValidator(),
+          numberValidator(),
+          symbolValidator()
+        ]],
+        confirmPassword: ['']
+      }, { validators: this.passwordMatchValidator });
+    });
+  }
+
+  passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  }
+
+  salvar(){
+    if(this.formulario.valid){
+      this.userService.editar(this.formulario.value).subscribe({
+        next: () => {
+          this.cancelar();
+        },
+        error: (error) => {
+          if (error.status === 400) {
+            const causes = error.error?.error?.causes || [];
+            causes.forEach((cause: any) => {
+              if (cause.cause === 'login') {
+                this.formulario.get('login')?.setErrors({ backendError: cause.message });
+              }
+            });
+          }
+        }
+      });
+    }
+  }
+  cancelar(){
+    this.router.navigate(['/users']);
+  }
 }
--- src/app/components/pages/user/list-user/list-user.component.ts
+++ src/app/components/pages/user/list-user/list-user.component.ts
@@ -31,6 +31,40 @@ export class ListUserComponent implements OnInit{
     });
   }
 
+  selectItem(id?: number){
+
+    this.id = id;
+
+    this.removeSelectedItems();
+
+    const element = document.getElementById(`line${id}`);
+    element!.className = element!.className +" row-selected";
+
+    if(this.id){
+      this.enableEditButton();
+      this.enableRemoveButton();
+    }
+  }
+
+  removeSelectedItems(){
+    const elements = document.getElementsByClassName('row-selected');
+
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].className = "";
+    }
+  }
+
+  enableEditButton(){
+    const btnEditar = document.getElementById('btnEditar') as HTMLButtonElement;
+
+    btnEditar.disabled = false;
+  }
+  enableRemoveButton(){
+    const btnExcluir = document.getElementById('btnExcluir') as HTMLButtonElement;
+
+    btnExcluir.disabled = false;
+  }
+
   incluir(){
     this.route.navigate(['users/create']);
   }
@@ -41,4 +75,5 @@ export class ListUserComponent implements OnInit{
   excluir(){
 
   }
+
 }
