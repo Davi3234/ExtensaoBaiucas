25012a99cc416730baae6a8e6462cf8b86eb2747
davi3234 <davi.fadriano@gmail.com>
2024-11-13 07:45:10 -0300

    feat: Criando pages de categoria: List, create e edit

---
+++ src/app/components/page/categoria/create-categoria/create-categoria.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-categoria',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-categoria.component.html',
+  styleUrl: './create-categoria.component.css'
+})
+export class CreateCategoriaComponent {
+
+}
---
+++ src/app/components/page/categoria/list-categoria/list-categoria.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-categoria',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-categoria.component.html',
+  styleUrl: './list-categoria.component.css'
+})
+export class ListCategoriaComponent {
+
+}
