211031d94a405b7e1ddc51575b5fae0e934f4e70
davi3234 <davi.fadriano@gmail.com>
2024-11-13 07:42:19 -0300

    feat: Criando page de produto, list, edit e create

---
+++ src/app/components/page/produto/create-produto/create-produto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-produto',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-produto.component.html',
+  styleUrl: './create-produto.component.css'
+})
+export class CreateProdutoComponent {
+
+}
---
+++ src/app/components/page/produto/list-produto/list-produto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-produto',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-produto.component.html',
+  styleUrl: './list-produto.component.css'
+})
+export class ListProdutoComponent {
+
+}
