5642e7e17faf7fd5f4158d311f3e25f879363533
davi3234 <davi.fadriano@gmail.com>
2024-11-13 07:48:20 -0300

    feat: Adicionando caminho de produto e categoria nas rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,12 @@ import { CreateUserComponent } from './components/pages/user/create-user/create-
 import { EditUserComponent } from './components/pages/user/edit-user/edit-user.component';
 import { LoginComponent } from './components/pages/auth/login/login.component';
 import { AuthGuardService } from './service/auth/can-active';
+import { ListProdutoComponent } from './components/page/produto/list-produto/list-produto.component';
+import { CreateProdutoComponent } from './components/page/produto/create-produto/create-produto.component';
+import { EditProdutoComponent } from './components/page/produto/edit-produto/edit-produto.component';
+import { ListCategoriaComponent } from './components/page/categoria/list-categoria/list-categoria.component';
+import { CreateCategoriaComponent } from './components/page/categoria/create-categoria/create-categoria.component';
+import { EditCategoriaComponent } from './components/page/categoria/edit-categoria/edit-categoria.component';
 
 export const routes: Routes = [
   {
@@ -24,6 +30,36 @@ export const routes: Routes = [
   {
     path: 'users/edit/:id',
     component: EditUserComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'products',
+    component: ListProdutoComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'products/create',
+    component: CreateProdutoComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'products/edit/:id',
+    component: EditProdutoComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'category',
+    component: ListCategoriaComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'category/create',
+    component: CreateCategoriaComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'category/edit/:id',
+    component: EditCategoriaComponent,
     canActivate: [AuthGuardService]
   },
   {
