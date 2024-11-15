dee2c3d8cf13e3f109011db9f7bc1b147c112731
Dan Ruan <danrsaquetti@gmail.com>
2024-11-15 15:14:34 -0300

    feat: Criando componente específica para a notificação

--- src/app/components/notification/notification-container/notification-container.component.ts
+++ src/app/components/notification/notification-container/notification-container.component.ts
@@ -1,19 +1,18 @@
 import { Component, ElementRef, Input, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { NotificationService } from '../../../service/notification/notification.service';
 import { Notification } from '../../../service/notification/notification';
+import { NotificationComponent } from "../notification/notification/notification.component";
 
 @Component({
   selector: 'app-notification-container',
   standalone: true,
-  imports: [],
+  imports: [NotificationComponent],
   templateUrl: './notification-container.component.html',
   styleUrl: './notification-container.component.css'
 })
 export class NotificationContainerComponent implements OnInit {
 
   @Input() notifications: Notification[] = []
-
-  @ViewChildren('notificationElement') notificationElements!: QueryList<ElementRef<HTMLDivElement>>;
 
   constructor(
     private notificationService: NotificationService
@@ -27,44 +26,11 @@ export class NotificationContainerComponent implements OnInit {
 
   addNotification(notification: Notification) {
     this.notifications.push(notification)
-    this.activeTimeoutToNotification(notification.id)
   }
 
-  onClickNotification(id: number) {
-    this.removeNotification(id)
-  }
+  removeNotification(id: number) {
+    const index = this.notifications.findIndex(({ id: notificationId }) => notificationId == id)
 
-  private activeTimeoutToNotification(id: number) {
-    const notificationElement = this.getElementNotification(id)
-
-    if (!notificationElement)
-      return
-
-    let count = 10
-
-    let timerCont = setInterval(() => {
-
-    }, 10)
-  }
-
-  private removeNotification(id: number) {
-    const notificationElement = this.getElementNotification(id);
-
-    if (notificationElement) {
-      notificationElement.nativeElement.classList.add('hidden')
-    }
-
-    setTimeout(() => {
-      const index = this.notifications.findIndex(({ id: notificationId }) => notificationId == id)
-
-      if (index < 0)
-        return
-
-      this.notifications.splice(index, 1)
-    }, 300)
-  }
-
-  private getElementNotification(id: number) {
-    return this.notificationElements.find(el => el.nativeElement.id === `notification-id-${id}`) || null
+    this.notifications.splice(index, 1)
   }
 }
--- src/app/components/notification/notification/notification/notification.component.ts
+++ src/app/components/notification/notification/notification/notification.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, QueryList, ViewChildren } from '@angular/core';
+import { Notification } from '../../../../service/notification/notification';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-notification',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './notification.component.html',
   styleUrl: './notification.component.css'
 })
-export class NotificationComponent {
+export class NotificationComponent implements OnInit {
 
+  @Input() notification!: Notification
+  @Output() remove = new EventEmitter<number>()
+
+  @ViewChildren('notificationElement') notificationElements!: QueryList<ElementRef<HTMLDivElement>>;
+
+  progress: number = 0
+  private intervalId: any;
+
+  ngOnInit(): void {
+    this.activeRemoveCounter()
+  }
+
+  onClick() {
+    this.deleteNotification()
+  }
+
+  private activeRemoveCounter() {
+    this.intervalId = setInterval(() => {
+      if (this.progress >= 1)
+        return this.deleteNotification();
+
+      this.progress += 0.002;
+    }, 10);
+  }
+
+  private deleteNotification() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+
+    const notificationElement = this.getCurrentElement()
+
+    notificationElement?.nativeElement.classList.add('hidden')
+
+    setTimeout(() => {
+      this.remove.emit(this.notification.id);
+    }, 300)
+  }
+
+  private getCurrentElement() {
+    return this.notificationElements.find(el => el.nativeElement.id === `notification-id-${this.notification.id}`) || null
+  }
 }
